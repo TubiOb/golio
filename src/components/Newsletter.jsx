dfc8e2c9a1b4bc5cbd295c841886decd34d186f8
TubiOb <Tubiobaloluwa@gmail.com>
2023-11-28 21:08:37 +0100

    correcting the testimonials

---
+++ src/components/Newsletter.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Newsletter = () => {
+  return (
+    <div className='bg-white lg:w-[70%] w-[90%] flex items-center py-3 px-4'>
+        <div className='rounded-md bg-blue-100 flex flex-col p-[5%] lg:p-[10%]'>
+            <div className=''>
+                <h4  className='font-extrabold text-3xl tracking-wide leading-6 '>Enter your e-mail address and get started for free</h4>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Newsletter
