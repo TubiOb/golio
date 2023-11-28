71706ca2e4c740ad0f51762ebd5586acac77bb40
TubiOb <Tubiobaloluwa@gmail.com>
2023-11-28 20:15:07 +0100

    finally got it to work

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -58,7 +58,7 @@ const Navigation = () => {
         </div>
 
 
-        <div className='absolute right-8 z-50 top-8 cursor-pointer lg:hidden' onClick={toggleMenu}>
+        <div className='absolute right-8 z-50 top-12 cursor-pointer lg:hidden' onClick={toggleMenu}>
                 {isMenuOpen 
                     ?
                     (<LiaWindowCloseSolid  size={30} className='text-black z-50' />)
