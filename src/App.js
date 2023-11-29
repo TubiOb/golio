cf7a6a60778899682a0a2327b2c5ea50ce7d165f
TubiOb <Tubiobaloluwa@gmail.com>
2023-11-29 07:03:21 +0100

    Second Page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Page2 from "./pages/Page2";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />}></Route>
+          <Route path="/page2" element={<Page2 />} ></Route>
         </Routes>
       </BrowserRouter>
     </div>
